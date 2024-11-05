deefdec133f37c1cf04321558953605ae375bdb2
Desmond Bush <desmondobush@gmail.com>
2024-11-04 21:54:26 -0500

    Updated resume and added icon

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={inter.className}>{children}</body>
       <Analytics />
     </html>
